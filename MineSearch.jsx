9a7207659d0f4aaf2085a1e07ef032bd61eccfc0
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-11-13 23:50:07 +0900

    1113 추가

--- MineSearch.jsx
+++ MineSearch.jsx
@@ -1,1 +1,30 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import Table from './Table';
+
+const initialState= {
+    tableData: [],
+    timer: 0,
+    result: '',
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+const MineSearch = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <>
+            <Form />
+            <div>{state.timer}</div>
+            <Table />
+            <div>{result}</div>
+        </>
+    )
+};
+
+export default MineSearch;
