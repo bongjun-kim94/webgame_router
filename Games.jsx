1573865c3fed2aebca10cc52213e2c3614a7d80a
bongjun-kim94 <mydlwm_s@naver.com>
2021-12-12 23:33:50 +0900

    1212 수정

--- Games.jsx
+++ Games.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 // import NumberBaseball from '';
 // import RSP from '';
 // import Lotto from '';
@@ -7,6 +7,13 @@ import { BrowserRouter } from 'react-router-dom';
 const Games = () => {
     return (
         <BrowserRouter>
+            <>
+                <Link to="/number-baseball">숫자야구</Link>
+                &npsp;
+                <Link to="/rock-scissors-paper">가위바위보</Link>
+                &npsp;
+                <Link to="/lotto-game">로또생성기</Link>
+            </>
             <>
                 <Route path="/number-baseball" component={NumberBaseball} />
                 <Route path="/rock-scissors-paper" component={RSP} />
